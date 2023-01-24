151296fa7007d90587b9a48f14c50cb418fce227
Andrea Vecino <andreavecino94@gmail.com>
2023-01-23 23:41:22 -0500

    clase 23/01/2023

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LibraryService } from '../services/library.service';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  authors: any;
 
-  constructor() {}
+  slideOps = {
+    initialSlide: 1,
+    slidesPerView: 3,
+    centeredSlides: true,
+    speed: 400
+  }
+  constructor(private libraryService: LibraryService) {}
+
+  ionViewDidEnter(){
+    this.libraryService.getAuthors().then( res => {
+      this.authors = res.data;
+      console.log(this.authors)
+    })
+  }
 
 }
